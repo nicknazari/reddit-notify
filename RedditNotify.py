a1e8500227640616887cb590542b86d120cf8b1a
Nick Nazari <nhn2915@gmail.com>
2018-07-10 20:00:39 -0400

    Added option for multiple keywords, added .gitignore

--- RedditNotify.py
+++ RedditNotify.py
@@ -1,24 +1,6 @@
 import time
-import smtplib
 import praw
-import email
 from slackclient import SlackClient
-
-# this func creates the chat function for slack
-def slack_message(message, channelToSend):
-
-	# token can be found at https://api.slack.com/custom-integrations/legacy-tokens
-	token = 'xoxp-362178409622-361284964306-361064429652-25e4424a9316132eefaa3b7807f2696d'
-	sc = SlackClient(token)
-
-	sc.api_call('chat.postMessage', channel=channelToSend,
-			    text=message, username='RedditGrabber',
-				icon_emoji=':robot_face:')
-
-# this connects to the reddit app to interact and use API
-reddit = praw.Reddit(client_id = 'gTLCf_9Do_yjEQ',
-					 client_secret = 'QzcbjiK0Slw0ZkfxnwX09NEKVjo',
-					 user_agent = 'my user agent')
 
 # all of the sent titles and links are stored here and checked before sending mail to avoid multiple messages of the same post. iterations are increased by 1 every search process
 doneTitles = []
@@ -26,8 +8,25 @@ doneLinks = []
 iterations = 1
 
 # define search keyword and subreddit here, these are the essential parts of the program
-keyword = '1070'
-subToSearch = 'hardwareswap'
+keywords = ['add any keywords here, you can insert multiple']
+subToSearch = 'subreddit to check'
+channelToSendMessage = 'channel to send messages to'
+
+# this func creates the chat function for slack
+def slack_message(message, channelToSend):
+
+	# token can be found at https://api.slack.com/custom-integrations/legacy-tokens
+	token = 'slack token'
+	sc = SlackClient(token)
+
+	sc.api_call('chat.postMessage', channel=channelToSend,
+			    text=message, username='bot username',
+				icon_emoji=':robot_face:')
+
+# this connects to the reddit app to interact and use API
+reddit = praw.Reddit(client_id = 'client id',
+					 client_secret = 'client secret',
+					 user_agent = 'user agent')
 
 # infinitely check the subreddit every two seconds
 while True:
@@ -52,15 +51,16 @@ while True:
 
 	# check for the keyword in every title, add it to the sendQueue (titles) and sendQueueL (links) ONLY IF IT CONTAINS THE KEYWORD
 	for title in titles:
-		if keyword in title:
-			sendQueue.append(title)
-			sendQueueL.append(links[titles.index(title)])
+		for keyword in keywords:
+			if keyword in title:
+				sendQueue.append(title)
+				sendQueueL.append(links[titles.index(title)])
 	
 	# for every entry in sendQueue list, check if its been sent already. if it hasnt then call the slack message func and send that title and link to the channel
 	for queueNum in range(0,len(sendQueue)):
 		if sendQueue[queueNum] not in doneTitles:
 
-			slack_message(sendQueue[queueNum] + '\n' + sendQueueL[queueNum], '#redditnotifications')
+			slack_message(sendQueue[queueNum] + '\n' + sendQueueL[queueNum], channelToSendMessage)
 			sent = True
 			print('Notification sent. ITERATION: ' + str(iterations))
 
